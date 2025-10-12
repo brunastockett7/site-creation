245169872b9fc738fbb9506f359330adf1b7fd76
brunastockett7 <bruna__stockett@outlook.com>
2025-10-12 15:31:01 -0800

    update

--- src/models/accountmodel.js
+++ src/models/accountmodel.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { query } = require("../db");
 
 /** Create a new account (prepared statement) */
